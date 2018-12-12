1cf55dd14e270c8c73da66751b7fe5dc0029d72f
ishan-mishra <im356@cornell.edu>
2018-12-12 16:13:33 -0500

    initial commit of data_analysis and refactored notebook

---
+++ data_analysis.py
@@ -0,0 +1,37 @@
+"""
+These are my data analysis functions used to download and process some temperature time series from Berkeley Earth.
+"""
+
+import numpy as np 
+import requests
+
+def generate_url(location):
+    url = f'http://berkeleyearth.lbl.gov/auto/Regional/TAVG/Text/{location.lower()}-TAVG-Trend.txt'
+    return url
+
+def download_data(location):
+    url = generate_url(location)
+    # Download the content of the URL
+    response = requests.get(url)
+    # Save it to a file
+    #with open("data.txt", 'w') as open_file:
+    #    open_file.write(response.text)
+        
+    #data = np.loadtxt("data.txt", comments="%")
+    #We will use generator to get data directly from a url
+    data = np.loadtxt(response.iter_lines(), comments="%")
+    
+    return data
+
+def moving_average(data,width):
+    moving_avg = np.full(data.size, np.nan)
+    for i in range(width, moving_avg.size - width):
+        moving_avg[i] = np.mean(data[i - width:i + width])
+    return moving_avg
+
+#test for moving average
+def test_moving_avg():
+    avg = moving_average(np.ones(1000),2)
+    assert np.all(np.isnan(avg[0:2]))
+    assert np.all(np.isnan(avg[-2:]))
+    assert np.allclose(avg[2:-2],1)
